6bb397c79cf5fc723695b21323abb97aefdcd999
Alexey Shemyakin <2alexeysh@gmail.com>
2019-06-13 11:49:00 +0300

    updates

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  validates_uniqueness_of :user_id, scope: [:votable_type, :votable_id]
+  validates :user_id, uniqueness: { scope: [:votable_type, :votable_id] }
   validates :value, presence: true, inclusion: { in: [-1, 1] }
 end
